2888a92111c7aa1994d1ab1ffb7165499d9c93c4
Randah <randah.abdullah12@gmail.com>
2024-07-03 03:33:22 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,34 +4,34 @@
     </title>
 </svelte:head>
 
-<div class="bg-[#f4f1f8] h-fit w-fit rounded-3xl grid grid-cols-4 gap-3">
-    
-            <button>%</button>
-            <button>√</button>
-            <button>CE</button>
-            <button>C</button>
+<div class="bg-[#f4f1f8] h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-3 font-semibold 
+ text-[#686f79] text-xl shadow-xl shadow-[rgb(139,148,167)]">
+    <div class="bg-blue-300 rounded-full col-span-4 h-12 flex items-center px-4 mb-2 text-white 
+    "> 123</div>
+            <button class="bg-[#f3f6fc]">%</button>
+            <button class="bg-[#f3f6fc]">√</button>
+            <button class="bg-[#f3f6fc]">CE</button>
+            <button class="bg-blue-900 text-white">C</button>
         
             <button>7</button>
             <button>8</button>
             <button>9</button>
-            <button>-</button>
+            <button class="bg-blue-800 text-white">-</button>
         
             <button>4</button>
             <button>5</button>
             <button>6</button>
-            <button>/</button>
+            <button class="bg-blue-700 text-white">/</button>
         
             <button>1</button>
             <button>2</button>
             <button>3</button>
-            <button>X</button>
+            <button class="bg-blue-600 text-white">X</button>
         
             <button>.</button>
             <button>0</button>
-            <button>=</button>
-
-
-            <button>+</button>
+            <button >=</button>
+            <button class="bg-blue-500 text-white">+</button>
         
         
     
