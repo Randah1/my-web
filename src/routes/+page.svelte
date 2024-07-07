8588374f40fb64511ab83073da946662eecd3d54
Randah <randah.abdullah12@gmail.com>
2024-07-07 05:35:50 +0300

    it is work now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,93 @@
+<script lang="ts">
+
+import SquareRoot from'$lib/icons/Squareroot.svelte';
+import BackSpace from'$lib/icons/BackSpace.svelte';
+
+let equation: string="";
+
+function addToEquation (value:string)
+{
+    equation +=value;
+}
+
+function restart()
+{
+    equation="";
+}
+function backspace()
+{
+    switch(equation.substring(equation.length - 3 , equation.length ))
+    {
+        case " * ":
+        case " + ":
+        case " - ":
+        case " / ":
+        equation=equation.substring(0,equation.length-3);
+       break;
+       case "100":     
+        equation=equation.substring(0,equation.length-4);
+      break;
+        default:
+        equation=equation.substring(0,equation.length-1);
+
+    }
+  
+}
+
+function solve()
+{
+equation = eval(equation);
+
+}
+</script>
+
 <svelte:head>
     <title>
-         الة حاسبة
+         آلة حاسبة
     </title>
 </svelte:head>
 
-<div class="bg-[#f4f1f8] h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-3 font-semibold 
- text-[#686f79] text-xl shadow-xl shadow-[rgb(139,148,167)]">
-    <div class="bg-blue-300 rounded-full col-span-4 h-12 flex items-center px-4 mb-2 text-white 
-    shadow-md shadow-[rgb(162,169,184)]"> 123</div>
-            <button class="bg-[#f3f6fc]">%</button>
-            <button class="bg-[#f3f6fc]">√</button>
-            <button class="bg-[#f3f6fc]">CE</button>
-            <button class="bg-blue-900 text-white">C</button>
+<div 
+   class="bg-[#f4f1f8] h-fit w-fit rounded-3xl grid grid-cols-4 gap-3 p-3 font-semibold 
+ text-[#686f79] text-xl shadow-xl shadow-[rgb(139,148,167)]  ">
+   <!--TODO fix overflowing numbers -->
+ <div class="bg-blue-300 rounded-full col-span-4 h-12 flex items-center px-4 mb-2 text-white 
+    shadow-md shadow-[rgb(162,169,184)] ">
+    {equation}
+ </div>
+          
+         <!--<button class="bg-[#f3f6fc] hover:bg-[#eceef3]"><SquareRoot />   </button> هذا لما كنت حاطة ازرار الجذر-->   
+    
+         <!--row1-->
+         <button on:click={restart} class="bg-[#f3f6fc] hover:bg-[#eceef3]">AC</button>
+        <button on:click={backspace} class="bg-[#f3f6fc] hover:bg-[#eceef3]">
+                <BackSpace/>
+            </button>
+        <button on:click={()=>addToEquation ('/100')} class="bg-[#f3f6fc]  hover:bg-[#eceef3]">%</button>
+        <button on:click={()=>addToEquation (' + ')} class="bg-blue-800 text-white  hover:bg-blue-800/80">+</button>
+           
+        <!--row2-->
         
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button class="bg-blue-800 text-white">-</button>
+            <button on:click={()=>addToEquation ('7')}>7</button>
+           
+            <button on:click={()=>addToEquation ('8')}>8</button>
+            <button on:click={()=>addToEquation ('9')}>9</button>
+            <button on:click={()=>addToEquation (' - ')} class="bg-blue-700 text-white  hover:bg-blue-700/80">-</button>
         
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button class="bg-blue-700 text-white">/</button>
+            <button on:click={()=>addToEquation ('4')}>4</button>
+            <button on:click={()=>addToEquation ('5')}>5</button>
+            <button on:click={()=>addToEquation ('6')}>6</button>
+            <button on:click={()=>addToEquation (' / ')} class="bg-blue-600 text-white  hover:bg-blue-600/80">/</button>
         
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button class="bg-blue-600 text-white">X</button>
+            <button on:click={()=>addToEquation ('1')}>1</button>
+            <button on:click={()=>addToEquation ('2')}>2</button>
+            <button on:click={()=>addToEquation ('3')}>3</button>
+            <button on:click={()=>addToEquation (' * ')} class="bg-blue-500 text-white  hover:bg-blue-500/80">X</button>
         
-            <button>.</button>
-            <button>0</button>
-            <button >=</button>
-            <button class="bg-blue-500 text-white">+</button>
+            <button on:click={()=>addToEquation ('.')}>.</button> 
+            <button on:click={()=>addToEquation ('0')}>0</button>
+            <button on:click={ solve } class="col-span-2 w-full">=</button>
+          
         
         
     
